02b3fed5d4442f939688eed473fe95ad4b4bb83e
Mlkz <maho.mamulashvili@gmail.com>
2022-10-04 11:49:42 -0400

    add create and search trie

--- trie.js
+++ trie.js
@@ -1,128 +1,83 @@
-const { __esModule } = require("live/lib/logging");
-
-// Without OOP we have to literally create objects manually one-by-one;
-let person1 = {
-    "name" : "Jonathan",
-    "heigh" : "5'8",
-    "weight" : 155,
-    "color" : "brown"
-}
-
-// Instead we can create class
-// name, heigh, weight, color are called instance variables and class attributes
-// This called incapsulation
-class Person {
-    constructor(name, heigh, weight, color) {
-        this.name = name;
-        this.heigh = heigh;
-        this.weight = weight;
-        this.color = color;
-    }
-
-    addWeight(num) {
-        this.weight += num;
-    }
-}
-
-
-let p1 = new Person('Jonathan', "5'8", 155, "brown");  // This guy is a new instance of the person class
-
-
-// The beauty is that we can create class with exclusive characteristics
-
-// take not ewhen you creat a class:
-    // 1. empty object is created
-    // 2. constracor method called and it is bound to an object
-    // 3. new object is returned agter constructor method is completed
-
-
 class Trie {
-    constructor(parent=null, children = [], val, complete=false) {
+    constructor(parent=null, val, children = [],  complete=false) {
         this.parent = parent;
-        this.children = children;
         this.val = val;
+        this.children = children;
         this.complete = complete;
     }
 
-    childVals() {
-        let retArr = [];
 
-        for (let i = 0; i < this.children.length; i++) {
-            retArr.push(this.children[i].val);
+    add(word) {
+        let node = this;
+        for (let i = 0; i < word.length; i++){
+            if (this._isChild(node, word[i]) === false){
+                let nextTrie = new Trie(node, word[i], [], false);
+                
+                node.children.push(nextTrie); 
+                node = nextTrie;
+            } else {
+                node = this._getChildNode(node, word[i]);
+            }
+            
+            if (i === word.length - 1) {
+                node.complete = true;
+            }
         }
-
-        return retArr;
     }
 
-    add(str) {
-        let curr = this;
-        let childValues = curr.childVals();
-        let next;
-        let tempIdx;
-        
-        for (let i = 0; i < str.length; i++) {
-            tempIdx = childValues.indexOf(str[i]);
 
-            if (tempIdx !== -1) {
-                curr = curr.children[tempIdx];
-                if (i === str.length - 1) {
-                    curr.complete = true;
-                }
+    buildWord() {
+        let name = "";
+        let node = this;
+
+        while (node.parent !== null) {
+            name = node.val + name
+            node = node.parent;
+        }
+
+        return name;
+    }
+
+
+    search(str) {
+        let stack = [this];
+        let name = [];
+
+        let i = 0;
+        while (stack.length > 0) {
+            let currChar = str[i];
+            let currNode = stack.pop();
+
+            if (currChar !== undefined && this._isChild(currNode, currChar)) {
+                let childNode = this._getChildNode(currNode, currChar);  // child with char as value
+                stack.push(childNode);
+                i++;
             } else {
-                if (i !== str.length -1) {
-                    next = new Trie (curr, [], str[i], false);
-                } else {
-                    next = new Trie (curr, [], str[i], true);
+                stack.push(...currNode.children.reverse());
+                if (currNode.complete) {
+                    name.push(currNode.buildWord());
                 }
-
-                curr.children.push(next);
-                curr = next;
-                childValues = curr.childVals();
             }
         }
 
+        return name;
     }
 
-    search(str) {
-        currChildren = this.children;
-        
+
+    _getChildNode(node, char) {
+        for (let i = 0; i < node.children.length; i++) {
+            let child = node.children[i];
+            if (child.val === char) return child;
+        }
     }
 
-    // // returns query that prints out childred on the parent node
-    // search(str) {
+    _isChild(node, val) {
+        for (let i = 0; i < node.children.length; i++) {
+            if (node.children[i].val === val) return true;
+        }
 
-    //     let tempStr = ""; // m
-    //     let retArr = [];
-    //     let curr = this
-    //     let childValues = curr.childVals();
-    //     let anchor;
-        
-    //     for (let i = 0; i < str.length; i++) {
-    //       let tempIdx = childValues.indexOf(str[i]);
-    //       if (tempIdx === -1) {
-    //         return -1;
-    //       } else {
-    //         curr = curr.children[tempIdx];
-    //         tempStr += curr.val;  
-    //       }
-    //     }
-        
-    //     anchor = curr;
-        
-    //     for (let i = 0; i < anchor.childValues().length; i++) {
-    //       retArr.push(tempStr);
-    //     }
-        
-        
-    //     for (let i = i; i < anchor.childValues().length; i++) {
-    //       while (curr.children.length !== 0) {
-    //         curr = curr.children[i];
-
-
-    //         // retArr[i].push(curr.children[i].val)
-    //       }
-    //     }
-    // }
+        return false;
+    }
 }
 
 module.exports = Trie;
